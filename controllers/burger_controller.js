616577f3f3ecc33bdc532f1b8cbc56b1fe8167a2
habibtaghavi08 <habibtaghavi08@gmail.com>
2020-04-21 22:56:02 -0500

    created burger_controller.js

---
+++ controllers/burger_controller.js
@@ -0,0 +1,10 @@
+// importing the burger model
+var burger_model = require("../models/burger_model")
+
+function burger_controller(app){
+
+}
+
+// exporting burger controller
+
+module.exports = burger_controller
