0c69de80cdf41cd03505d92358949809afc2e999
habibtaghavi08 <habibtaghavi08@gmail.com>
2020-04-21 22:42:42 -0500

    created and finished server.js

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
-// creating server
+// creating server  and Url path 
 var express = require("express")
 
 var app = express()
+
+// creating port
+var port = process.env.PORT || 8000
+
+// initalizing the handlebars setup
+var expressHandleBars = require("express-handlebars")
+app.engine("handlebars", expressHandleBars({defaultLayout:"main" }))
+
+// setting the view engine to handlebars
+app.set("view engine", "handlebars")
+
