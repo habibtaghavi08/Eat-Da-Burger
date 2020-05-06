9867334af7156d1b24c74afce9ccc773bb73686a
habibtaghavi08 <habibtaghavi08@gmail.com>
2020-05-05 22:30:00 -0500

    updated css and added console log for orm file

--- config/orm.js
+++ config/orm.js
@@ -17,6 +17,7 @@ update:function(tableName,columns,values,cbModel){
     var statement = connection.query("update ?? set ?? = ? where ?? = ?",[tableName, columns[0],values[0],columns[1], values[1]],function(error,burgerData){
         cbModel(burgerData)
     })
+    console.log(statement.sql)
 }
 
 
