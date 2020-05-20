2daeb8dabf91401cb1ce9588b7befa687fbbdc0e
habibtaghavi08 <habibtaghavi08@gmail.com>
2020-05-19 23:42:31 -0500

    added new line of code for mongoose to be able to deploy yo Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,5 @@
  var mysql=require("mysql")
 
-// var connection = mysql.createConnection({
-//     host:"localhost",
-//     port:3306,
-//     user: "habib",
-//     password:"password",
-//     database:"burgers_db"
-// })
 var connection = mysql.createConnection({
     host: process.env.DB_HOST,
     port:3306,
